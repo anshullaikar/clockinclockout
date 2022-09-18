3d707f52a744d231a9529712f7b4be17eefd9b66
anshul <anshullaikar@gmail.com>
2022-09-19 02:02:40 +0530

    added some server files

--- timesheet-server/index.ts
+++ timesheet-server/index.ts
@@ -1,27 +1,61 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const superstruct = require("superstruct");
+import express from "express";
+import dotenv from "dotenv";
+import * as superstruct from "superstruct";
+import fs from "fs";
+import { join, dirname } from 'path'
+import { Low, JSONFile } from 'lowdb'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Use JSON file for storage
+const file = join(__dirname, 'db.json')
+const adapter = new JSONFile(file)
+const db:any = new Low(adapter)
+
+// Read data from JSON file, this will set db.data content
+await db.read()
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
+
 const port = process.env.PORT;
-//connect lowdb
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
 
-const adapter = new FileSync("db.json");
-const db = low(adapter);
 
-db.defaults({ posts: [], user: {} }).write();
+// {
+//     "id":1,
+//     "type":"subtask/main task",
+//     "name":"Add task to timesheet",
+//     "priority":"high",
+//     "estimatedHours":3,
+//     "taskLinkedTo":1
+// }
 
-app.get("/add", (req:any, res:any) => {
+db.data ||= { tasks: [] }   
+
+app.get("/create", (req: any, res: any) => {
+    const { id, type, name, priority, estimatedHours, taskLinkedTo } = req.body;
+    console.log({ id, type, name, priority, estimatedHours, taskLinkedTo });
+    db.data.tasks
+        .push({ id, type, name, priority, estimatedHours, taskLinkedTo })
+        .write();
+    res.send("Success");
+});
+app.get("/delete", (req: any, res: any) => {
+    res.send("Express + TypeScript Server");
+});
+app.get("/edit", (req: any, res: any) => {
+    res.send("Express + TypeScript Server");
+});
+app.get("/read", (req: any, res: any) => {
     res.send("Express + TypeScript Server");
 });
 
 //add, remove, update, delete tasks
 //store deleted tasks in other db
-
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at https://localhost:${port}`);
