d9b77b32d472ec4d6b8ac5af188e9256c33fbd7e
anshul <anshullaikar@gmail.com>
2022-09-18 12:26:39 +0530

    added files to server

--- timesheet-server/index.ts
+++ timesheet-server/index.ts
@@ -1,15 +1,27 @@
-const express = require("express")
-const dotenv  = require("dotenv")
-const superstruct = require("superstruct")
+const express = require("express");
+const dotenv = require("dotenv");
+const superstruct = require("superstruct");
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
+//connect lowdb
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
 
-app.get("/", (req, res) => {
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+db.defaults({ posts: [], user: {} }).write();
+
+app.get("/add", (req:any, res:any) => {
     res.send("Express + TypeScript Server");
 });
+
+//add, remove, update, delete tasks
+//store deleted tasks in other db
+
 
 app.listen(port, () => {
     console.log(`[server]: Server is running at https://localhost:${port}`);
